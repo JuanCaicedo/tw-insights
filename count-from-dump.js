957b85b985ead06dcfb8792ec49e3f61ed35e00e
Juan Caicedo <retiredcanadianpoet@gmail.com>
2018-03-26 12:23:54 -0500

    extract require all

--- count-from-dump.js
+++ count-from-dump.js
@@ -4,9 +4,11 @@ const R = require('ramda')
 const glob = require('glob')
 const globP = Bluebird.promisify(glob)
 
+const requireAll = R.map(require)
+
 const loadAllTweets = async () => {
   const files = await globP('./tw-data/data/js/tweets/*.js')
-  files.map(file => require(file))
+  requireAll(files)
 }
 
 const getTweetsCount = R.pipe(R.values, R.map(R.length), R.sum)
