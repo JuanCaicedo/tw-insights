963f8f6e91c0308a3080a53b5891730b0b2c5506
Juan Caicedo <retiredcanadianpoet@gmail.com>
2018-03-26 17:17:24 -0500

    use caching for better testing

--- src/ms.js
+++ src/ms.js
@@ -8,10 +8,14 @@ const host = 'westus.api.cognitive.microsoft.com'
 const path = '/text/analytics/v2.0/languages'
 const url = `https://${host}${path}`
 
+const pickLanguages = R.pipe(R.prop('data'), R.prop('documents'))
+
 const getLanguages = data => {
-  return axios.post(url, data, {
-    headers: { 'Ocp-Apim-Subscription-Key': MS_ACCESS_KEY },
-  })
+  return axios
+    .post(url, data, {
+      headers: { 'Ocp-Apim-Subscription-Key': MS_ACCESS_KEY },
+    })
+    .then(pickLanguages)
 }
 
 module.exports = {
