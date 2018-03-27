44697b83a792149732689a5dabc53076fd61eda6
Juan Caicedo <retiredcanadianpoet@gmail.com>
2018-03-27 16:10:14 -0500

    simplify placeholder command

--- src/commands/add-languages.ts
+++ src/commands/add-languages.ts
@@ -1,29 +1,12 @@
 import {Command, flags} from '@oclif/command'
 
 export default class AddLanguages extends Command {
-  static description = 'describe the command here'
+  static description = 'Analyze tweets to determine its main language'
 
   static examples = [
-    `$ tw-insights add-languages
-hello world from ./src/add-languages.ts!
-`,
+    `$ echo '{ text: "hello world" }' | tw-insights add-languages`,
   ]
 
-  static flags = {
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
-    force: flags.boolean({char: 'f'}),
-  }
-
-  static args = [{name: 'file'}]
-
   async run() {
-    const {args, flags} = this.parse(AddLanguages)
-
-    const name = flags.name || 'world'
-    this.log(`hello ${name} from ${__filename}!`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
-    }
   }
 }
