d7c325ddf1908b568061d0974303dbae24580b78
Juan Caicedo <retiredcanadianpoet@gmail.com>
2018-03-27 12:11:45 -0500

    return docs to stdout

--- scripts/add-language.js
+++ scripts/add-language.js
@@ -1,24 +1,28 @@
+const R = require('ramda')
 const oboe = require('oboe')
 const Bluebird = require('bluebird')
 
-const readStream = input => {
-  let count = 0
-  return new Bluebird(resolve => {
-    oboe(input)
-      .node('{text}', tweet => {
-        count += 1
-      })
-      .fail(err => {
-        console.error('err', err)
-      })
+const toStdOut = R.pipe(JSON.stringify, console.log)
 
-    input.on('end', () => resolve(count))
-  })
+const readStream = (input, max, cb) => {
+  let buffer = []
+  oboe(input)
+    .node('{text}', tweet => {
+      buffer.push(tweet)
+      if (buffer.length >= max) {
+        cb(buffer)
+        buffer = []
+      }
+    })
+    .fail(err => {
+      console.error('err', err)
+    })
+
+  input.on('end', () => cb(buffer))
 }
 
 const main = async () => {
-  const count = await readStream(process.stdin)
-  console.log('count', count)
+  readStream(process.stdin, 100, docs => R.map(toStdOut, docs))
 }
 
 main()
