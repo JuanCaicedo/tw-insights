706e7b1ddaf170aaeaee6cccd918f0bd9c203c41
Juan Caicedo <retiredcanadianpoet@gmail.com>
2018-03-27 12:46:29 -0500

    full script for adding language to object

--- scripts/add-language.js
+++ scripts/add-language.js
@@ -1,6 +1,11 @@
 const R = require('ramda')
 const oboe = require('oboe')
 const Bluebird = require('bluebird')
+const replay = require('replay')
+
+const { getLanguages } = require('../src/ms')
+const { logErr } = require('../src/logging')
+const mapIndexed = R.addIndex(R.map)
 
 const toStdOut = R.pipe(JSON.stringify, console.log)
 
@@ -21,8 +26,31 @@ const readStream = (input, max, cb) => {
   input.on('end', () => cb(buffer))
 }
 
+const renameKeys = R.curry((keysMap, obj) =>
+  R.reduce(
+    (acc, key) => R.assoc(keysMap[key] || key, obj[key], acc),
+    {},
+    R.keys(obj)
+  )
+)
+const renameId = renameKeys({ id_str: 'id' })
+const sanitize = R.pipe(R.pick(['id_str', 'text']), renameId)
+
+const addLanguage = tweets => languagesForTweets => {
+  return mapIndexed((tweet, idx) => {
+    const languages = R.nth(idx, languagesForTweets)
+    return R.merge(tweet, { languages })
+  }, tweets)
+}
+
 const main = async () => {
-  readStream(process.stdin, 100, docs => R.map(toStdOut, docs))
+  readStream(process.stdin, 100, tweets => {
+    const sanitized = R.map(sanitize, tweets)
+    getLanguages({ documents: sanitized })
+      .then(addLanguage(tweets))
+      .then(results => R.map(toStdOut, results))
+      .catch(err => logErr(err))
+  })
 }
 
 main()
