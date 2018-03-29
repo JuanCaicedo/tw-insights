e798d4d3a9fbff6a68ae245d7fffd1f730e1a15f
Juan Caicedo <retiredcanadianpoet@gmail.com>
2018-03-29 11:39:11 -0500

    fix load test script

--- scripts/read-tweets.js
+++ scripts/read-tweets.js
@@ -1,8 +1,11 @@
 const R = require('ramda')
 const { loadAllTweets } = require('../src/load')
+const path = require('path')
 
 const main = async () => {
-  const rawTweets = await loadAllTweets()
+  const rawTweets = await loadAllTweets(
+    path.join(__dirname, '../tw-data/data/js/tweets/*.js')
+  )
   R.map(tweet => console.log(JSON.stringify(tweet)), rawTweets)
 }
 
