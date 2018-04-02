8a91c47c39f0bb8358117ca7a23df54bdd345180
Juan Caicedo <retiredcanadianpoet@gmail.com>
2018-04-02 12:25:50 -0500

    run all command

--- src/commands/run-all.ts
+++ src/commands/run-all.ts
@@ -1,29 +1,69 @@
-import {Command, flags} from '@oclif/command'
+import { Command } from '@oclif/command'
+import * as R from 'ramda'
+import * as path from 'path'
+import {
+  isAbsolutePath,
+  sanitize,
+  addLanguage,
+  addSentiment,
+  addKeyPhrases,
+} from '../utils'
+import { loadAllTweets } from '../load'
+import { getLanguages, getSentiments, getKeyPhrases } from '../ms'
+
+const MAX_MS_BODY_SIZE = 1000
+const intoBatches = R.splitEvery(MAX_MS_BODY_SIZE)
+const getLanguagesOne = R.pipe(R.map(sanitize), sanitized =>
+  getLanguages({ documents: sanitized })
+)
+const getLanguagesAll = R.map(getLanguagesOne)
+const getSentimentsOne = R.pipe(R.map(sanitize), sanitized =>
+  getSentiments({ documents: sanitized })
+)
+const getSentimentsAll = R.map(getSentimentsOne)
+const getKeyPhrasesOne = R.pipe(R.map(sanitize), sanitized =>
+  getKeyPhrases({ documents: sanitized })
+)
+const getKeyPhrasesAll = R.map(getKeyPhrasesOne)
 
 export default class RunAll extends Command {
-  static description = 'describe the command here'
+  static description = 'Run all MS azure text analysis tasks on a tweet archive'
 
-  static examples = [
-    `$ tw-insights run-all
-hello world from ./src/run-all.ts!
-`,
+  static examples = [`$ tw-insights run-all ./tw-data`]
+
+  static args = [
+    {
+      name: 'path-to-archive',
+      required: true,
+      description: 'Relative path to twitter archive',
+    },
   ]
 
-  static flags = {
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
-    force: flags.boolean({char: 'f'}),
-  }
-
-  static args = [{name: 'file'}]
+  toStdOut = msg => R.pipe(JSON.stringify, this.log)(msg)
 
   async run() {
-    const {args, flags} = this.parse(RunAll)
+    const { args } = this.parse(RunAll)
+    const pathToArchive = args['path-to-archive']
 
-    const name = flags.name || 'world'
-    this.log(`hello ${name} from ${__filename}!`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
-    }
+    const tweetPath = isAbsolutePath(pathToArchive)
+      ? path.join(pathToArchive, '/data/js/tweets/*.js')
+      : path.join(process.cwd(), pathToArchive, '/data/js/tweets/*.js')
+    const rawTweets = await loadAllTweets(tweetPath)
+    const batches = intoBatches(rawTweets)
+    const languageBatches = await Promise.all(getLanguagesAll(batches))
+    const withLanguage = addLanguage(rawTweets)(R.unnest(languageBatches))
+    const langBatches = intoBatches(withLanguage)
+    const sentimentBatches = await Promise.all(getSentimentsAll(langBatches))
+    const withSentiment = addSentiment(withLanguage)(
+      R.flatten(sentimentBatches)
+    )
+    const keyPhraseBatches = await Promise.all(getKeyPhrasesAll(langBatches))
+    const withKeyPhrase = addKeyPhrases(withSentiment)(
+      R.unnest(keyPhraseBatches)
+    )
+
+    const result = withKeyPhrase
+
+    R.map(this.toStdOut, result)
   }
 }
