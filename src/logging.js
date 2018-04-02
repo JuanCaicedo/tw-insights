5a195231ce67494ba45d0d62911d7ad6b417c050
Juan Caicedo <retiredcanadianpoet@gmail.com>
2018-04-02 12:53:13 -0500

    better logging

--- src/logging.js
+++ src/logging.js
@@ -1,11 +1,13 @@
 const logErr = err => {
   if (err.response) {
     console.error('err.response.data', err.response.data)
+  } else if (err.message) {
+    console.error('err.message', err.message)
+    console.error('err.stack', err.stack)
   } else if (err.request) {
     console.error('err.request', err.request)
   } else {
-    console.error('err.message', err.message)
-    console.error('err.stack', err.stack)
+    console.error('err', err)
   }
 }
 
